b8033f84810ecb7e29b643aca820d67e1f764d67
AdvancedAntiSkid <abel.herfort@gmail.com>
2024-09-22 19:21:09 +0200

    chore(concurrent): add missing null assertations to future exception

--- src/main/java/com/atlas/futura/concurrent/future/FutureExecutionException.java
+++ src/main/java/com/atlas/futura/concurrent/future/FutureExecutionException.java
@@ -1,9 +1,10 @@
 package com.atlas.futura.concurrent.future;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
- * An exception, that represents if a {@link Future} has completed
- * unsuccessfully / with an exception. The original cause can be retrieved
- * with {@link #getCause()}.
+ * An exception, that represents if a {@link Future} has completed unsuccessfully with or without an exception.
+ * The original cause can be retrieved with {@link #getCause()}.
  */
 public class FutureExecutionException extends Exception {
     /**
@@ -14,37 +15,43 @@ public class FutureExecutionException extends Exception {
 
     /**
      * Initialize the future execution exception.
-     * @param message exception cause description
+     *
+     * @param message the exception cause description
      */
-    public FutureExecutionException(String message) {
+    public FutureExecutionException(@NotNull String message) {
         super(message);
     }
 
     /**
      * Initialize the future execution exception.
-     * @param message exception cause description
-     * @param cause exception cause error
+     *
+     * @param message the exception cause description
+     * @param cause the exception cause error
      */
-    public FutureExecutionException(String message, Throwable cause) {
+    public FutureExecutionException(@NotNull String message, @NotNull Throwable cause) {
         super(message, cause);
     }
 
     /**
      * Initialize the future execution exception.
-     * @param cause exception cause error
+     *
+     * @param cause the exception cause error
      */
-    public FutureExecutionException(Throwable cause) {
+    public FutureExecutionException(@NotNull Throwable cause) {
         super(cause);
     }
 
     /**
      * Initialize the future execution exception.
+     *
      * @param message exception cause description
      * @param cause exception cause error
      * @param enableSuppression whether suppression is enabled or disabled
      * @param writableStackTrace whether the stack trace should be writable
      */
-    public FutureExecutionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    public FutureExecutionException(
+        @NotNull String message, @NotNull Throwable cause, boolean enableSuppression, boolean writableStackTrace
+    ) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 }
