f77615fb6085854ed121786fb0bfab5dd2cd7161
AdvancedAntiSkid <abel.herfort@gmail.com>
2024-09-22 18:40:30 +0200

    feat: set up java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("java")
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 group = "com.atlas"
 version = "1.0-SNAPSHOT"
 
